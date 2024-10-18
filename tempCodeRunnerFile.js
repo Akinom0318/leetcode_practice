e064f64f0f228e2a797c428835493bbc0cae0bce
Akinom <57698715+Akinom0318@users.noreply.github.com>
2024-10-18 16:34:22 +0800

    solved (#20)

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+        // let string_possible_array = JSON.stringify(possible_array);
+        // if(string_possible_array.indexOf(JSON.stringify(sub_array)) !== -1){
+        //     return;
+        // }
